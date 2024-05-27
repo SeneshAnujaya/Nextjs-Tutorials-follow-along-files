2de8e54b743903671cd6060adf782430e47a3bd0
SeneshAnujaya <senesh9637@gmail.com>
2024-05-27 17:34:30 +0530

    add task create edit delete functionlity

--- app/query/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const QueryPage = () => {
-  return (
-    <div>Query page</div>
-  )
-}
-
-export default QueryPage
---
+++ components/TaskForm.jsx
@@ -0,0 +1,16 @@
+import { createTask } from "@/utils/actions"
+
+const TaskForm = () => {
+  return (
+    <form action={createTask} className="flex">
+      <div className="join w-full">
+        <input type="text" className="input input-bordered join-item w-full" placeholder="type here" name="content" required/>
+      </div>
+      <button type="submit" className="btn btn-primary join-item">
+        create task
+      </button>
+    </form>
+  )
+}
+
+export default TaskForm
